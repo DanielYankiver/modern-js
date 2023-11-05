e6deddc78a24b8d53ec0e6c7ba4bbef70eec4218
Daniel Yankiver <69213153+DanielYankiver@users.noreply.github.com>
2023-11-05 11:10:09 -0500

    played with shortcuts

---
+++ 01-variables-data-types/02-comments-shortcuts/script.js
@@ -0,0 +1,33 @@
+// single line comment
+
+// console.log(100);
+
+console.log('hello earthlings');
+
+/* 
+multi-line comment
+console.log(20, 'we come in peace', true);
+
+const x = 99;
+
+console.log(x);
+*/
+
+// clg - easy type
+console.log('first');
+
+console.error('Alert');
+
+console.warn('Warning');
+
+console.table({ name: 'Daniel', email: 'daniel@gmail.com' });
+
+console.group('simple');
+console.log('first');
+console.error('Alert');
+console.warn('Warning');
+console.groupEnd();
+
+const styles = 'padding: 10px; background-color: white; color: green;';
+
+console.log('%cHello Earthlings', styles);
